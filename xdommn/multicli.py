f2a345d5dee747b05870c5842d42d7d3a3fd0c9e
Y.Jace Liu <yang.jace.liu@linux.com>
2017-10-11 18:20:39 +0800

    Use cross domain shell
    Signed-off-by: Y.Jace Liu <yang.jace.liu@linux.com>

--- xdommn/multicli.py
+++ xdommn/multicli.py
@@ -1,26 +1,67 @@
 #!/usr/bin/env python
 
+import readline
+from cmd import Cmd
+
 from mininet.cli import CLI
 from mininet.log import info
 
-def MCLI(**networks):
-    prompt = 'crossdomain> '
-    while True:
-        cmd = raw_input(prompt)
-        if cmd == 'quit':
-            break
-        if cmd == 'help':
-            info('ls     list all available networks\n')
-            info('<net>  switch the cli to <net>\n')
-            info('quit   quit the cli and close the simulator\n')
-            info('help   print this list\n')
-        elif cmd == 'ls':
-            info('\n'.join(sorted(networks.keys())))
-            info('\n')
-        elif cmd in networks.keys():
-            CLI.prompt = '%s> ' % cmd
-            info('***** Running CLI for %s *****\n' % cmd)
-            net = networks[cmd]
+
+class CrossDomainCmd(Cmd):
+    def __init__(self, completekey='tab', stdin=None, stdout=None, **networks):
+        Cmd.__init__(self, completekey, stdin, stdout)
+        self.intro = "A console to help controll crossdomain networks\n"
+        self.prompt = "Cross Domain >"
+        self.networks = networks
+
+    def do_ls(self, arg):
+        print('\n'.join(sorted(self.networks.keys())))
+        print('\n')
+
+    def help_ls(self):
+        print("List all available networks\n")
+
+    def do_exit(self, arg):
+        return True
+
+    def help_exit(self):
+        print("Exit the console")
+
+    do_quit = do_exit
+
+    help_quit = help_exit
+
+    do_EOF = do_exit
+
+    help_EOF = help_exit
+
+    def do_net(self, arg):
+        domain_name = arg.split(' ')[0]
+        if domain_name in self.networks.keys():
+            print('***** Running CLI for %s *****\n' % domain_name)
+            net = self.networks[domain_name]
             CLI(net)
         else:
-            info('Unknown command: %s\n' % cmd)
+            print("***** Cannot find domain named \"" + domain_name +
+                  "\" *****")
+
+    def help_net(self):
+        print("Choose a domain and connect its mininet shell")
+        print("format: net <domain name>")
+
+    def do_execute(self, arg):
+        # TODO: do some magic
+        pass
+
+    def help_execute(self):
+        print("Execute a command on a node")
+        print("format: execute <domain_name:node_name> <command>")
+
+
+def MCLI(**networks):
+    cmd = CrossDomainCmd(networks)
+    cmd.cmdloop()
+
+
+if __name__ == '__main__':
+    MCLI()
